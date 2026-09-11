416617acaf25771fd0a8be91ceaec9d83d680430
Radzhy Guseinov <54052086+Radzhyyy@users.noreply.github.com>
2026-09-10 21:12:05 -0500

    Add: is-anagram - submission-80

---
+++ Data Structures & Algorithms/is-anagram/submission-80.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashmapS = {}
+        hashmapT = {}
+
+
+
+        for i in s:
+            hashmapS[i] = hashmapS.get(i, 0) + 1
+
+        for i in t:
+            hashmapT[i] = hashmapT.get(i, 0) + 1
+
+        if hashmapT == hashmapS:
+            return True
+
+        return False 
+
+
